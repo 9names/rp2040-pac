672952894decbf839f7440098fe185bbb9a3ff29
9names <60134748+9names@users.noreply.github.com>
2021-01-23 14:43:01 +1100

    Move docs folder manually

---
+++ docs/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["aligned"] = [{"text":"impl&lt;A, T&gt; Display for Aligned&lt;A, T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;A: Alignment,<br>&nbsp;&nbsp;&nbsp;&nbsp;T: Display,&nbsp;</span>","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- github_pages_doc_gen.sh
+++ github_pages_doc_gen.sh
@@ -3,5 +3,6 @@ set -x
 set -e
 
 rm docs -rf
-cargo doc --target-dir docs
+cargo doc
+mv target/doc docs
 echo '<html><head><meta http-equiv="refresh" content="0; url=./rp2040_pac/index.html"></head><body></body></html>' > docs/index.html
