8b18f90f0065e4b88a5d34d179ae27acd583be8c
9names <60134748+9names@users.noreply.github.com>
2021-01-23 14:20:27 +1100

    Convert newlines into cargo::doc compatible fmt

--- update.sh
+++ update.sh
@@ -13,5 +13,10 @@ mkdir src
 svd2rust -i svd/rp2040.svd 
 form -i lib.rs -o src
 rm lib.rs
+# Original svd has \n (two chars) in it, whch gets converted to "\n" and "\\n" by svd2rust
+# If we convert them to newline characters in the SVD, they don't turn up in markdown so online docs suffer
+# So, convert both \\n and \n to [spc] [spc] [newline], then strip the spaces out if there are consecutive [newlines]
+# This means that by the time we're in docs.rs markdown, \n\n becomes new paragraph, and \n becomes a new line
+find src -name '*.rs' -exec sed -i -e 's/\\\\n/\\n/g' -e 's/\\n/  \n/g' -e 's/  \n  \n/\n\n/g' {} \;
 cargo fmt
 rustfmt build.rs
